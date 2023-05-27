4bc963908c96fe140d11cad9d3062d71da3db1b7
sunshanpeng <sunshanpeng@outlook.com>
2023-05-27 16:17:23 +0800

    feat: 增加token时间

--- service/src/user/jwt.ts
+++ service/src/user/jwt.ts
@@ -4,7 +4,7 @@ import jwt from 'jsonwebtoken'
 const secret = 'apeng' // replace with your own secret key
 
 const options = {
-  expiresIn: '1h', // token will expire in 1 hour (you can set this to whatever you want)
+  expiresIn: '12h', // token will expire in 1 hour (you can set this to whatever you want)
 }
 
 export function sign(username: string): string {
@@ -21,6 +21,6 @@ export function verify(token: string): any {
   }
   catch (err) {
     console.log(err)
-    throw new Error('Error: token 校验失败 | No access rights')
+    throw new Error('token 校验失败, 请重新登录')
   }
 }
